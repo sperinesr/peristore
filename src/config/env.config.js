06a5ce09b2d64d04274bb2d112ef45d1aee53e9a
sperinesr <sebastianperinesr@gmail.com>
2024-09-08 21:36:53 -0300

    se elimino el puerto por defecto

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT || 8080,
+    port: process.env.PORT,
     mongoURI: process.env.MONGODB_URI,
     // secretKey: process.env.SECRET_KEY,
 };
