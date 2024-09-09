1ab0ef90e5455a2548ec562c1dce3eda60204983
sperinesr <sebastianperinesr@gmail.com>
2024-09-08 21:44:02 -0300

    puerto para render

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 module.exports = {
-    port: process.env.PORT,
+    port: process.env.PORT || 3000,
     mongoURI: process.env.MONGODB_URI,
     // secretKey: process.env.SECRET_KEY,
 };
