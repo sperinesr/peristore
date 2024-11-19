f7b542b8adf6ea17bd82f2c6ea216bdb354f4394
Perines Rojas <sebastian.perines@dgac.gob.cl>
2024-11-19 14:37:16 -0300

    se cambio la cuenta de transferencia y se agrego el boton de copiar

--- src/public/js/scripts.js
+++ src/public/js/scripts.js
@@ -4,12 +4,30 @@ function mostrarDatosTransferencia() {
         html: `
             <strong>Nombre:</strong> Sebastian Perines <br>  
             <strong>RUT:</strong> 18.924.939-K <br> 
-            <br>  
-            <strong>Banco:</strong> Banco BCI <br>
+            <strong>Banco:</strong> Tenpo Prepago <br>
             <strong>Tipo Cuenta:</strong> Cuenta Vista<br>
-            <strong>N° Cuenta:</strong> 23501693 <br>
+            <strong>N° Cuenta:</strong> 111118924939 <br>
         `,
         icon: 'info',
-        confirmButtonText: 'Cerrar'
+        showCancelButton: true,
+        confirmButtonText: 'Copiar Datos',
+        cancelButtonText: 'Cerrar',
+        didOpen: () => {
+            const copiarButton = document.querySelector('.swal2-confirm');
+            copiarButton.addEventListener('click', () => {
+                const datos = `
+                    Nombre: Sebastian Perines
+                    RUT: 18.924.939-K
+                    Banco: Tenpo Prepago
+                    Tipo Cuenta: Cuenta Vista
+                    N° Cuenta: 111118924939
+                `;
+                navigator.clipboard.writeText(datos).then(() => {
+                    Swal.fire('¡Datos copiados!', 'Los datos fueron copiados al portapapeles.', 'success');
+                }).catch((err) => {
+                    Swal.fire('Error', 'Hubo un problema al copiar los datos.', 'error');
+                });
+            });
+        }
     });
 }
