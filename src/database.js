eabc12e43066aea6e3ebc3349562953d32c839d2
sperinesr <sebastianperinesr@gmail.com>
2025-05-17 19:17:17 -0400

    se elimino una variable de entorno de prueba

--- src/database.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose")
-
-mongoose.connect("mongodb+srv://sebastianperinesr:coderhouse@cluster0.b94vd8g.mongodb.net/test?retryWrites=true&w=majority&appName=Cluster0")
-    .then(() => console.log("Conectado a la BD"))
-    .catch(error => console.log(error))
